547ca5059f9aadfc36403aeec6c0988ee08cd67b
Tobi Balogun <98078707+tobisupreme@users.noreply.github.com>
2023-02-20 02:06:37 +0100

    fix: improve task dto validation

--- src/tasks/dto/query-tasks.dto.ts
+++ src/tasks/dto/query-tasks.dto.ts
@@ -1,6 +1,12 @@
 import { TaskStatus } from '@prisma/client';
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 
 export class QueryTaskDto {
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status?: TaskStatus;
+
+  @IsOptional()
+  @IsString()
   search?: string;
 }
